727361c8d7533f1b780e22664690f9ccbeec490e
OSAFUNE <pcjidouka@gmail.com>
2026-02-08 16:07:50 +0900

    Update ads configuration

--- components/HeaderAds.tsx
+++ components/HeaderAds.tsx
@@ -2,24 +2,88 @@ import React from 'react';
 
 const AMAZON_LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/320px-Amazon_logo.svg.png";
 
-// 商品情報（ユーザー様にて画像URLをSiteStripeから取得して差し替えてください）
+// 商品情報（楽天おすすめ情報）
 const AD_ITEMS = [
-    { url: "https://amzn.to/45Jtjia", title: "Amazon 緑茶ラベルレス 500ml", image: "" },
-    { url: "https://amzn.to/4a7WrB0", title: "DANISH ENDURANCE ソックス", image: "" },
-    { url: "https://amzn.to/4tf8gy8", title: "Amazon商品", image: "" },
-    { url: "https://amzn.to/4aayIAc", title: "Amazon商品", image: "" },
-    { url: "https://amzn.to/4buP4Gc", title: "Amazon商品", image: "" },
-    { url: "https://amzn.to/4aoR4ys", title: "TRUSCO 新聞紙・雑誌ストッカー", image: "" },
-    { url: "https://amzn.to/4qsIy6J", title: "JAKS ホエイプロテイン", image: "" },
-    { url: "https://amzn.to/4k5fBMg", title: "ハウス とろうま牛角煮カレー", image: "" },
-    { url: "https://amzn.to/4qPUQa1", title: "熊野油脂 馬油シャンプー", image: "" },
-    { url: "https://amzn.to/46n6HUG", title: "熊野油脂 馬油コンディショナー", image: "" },
-    { url: "https://amzn.to/3LXL9Hw", title: "TRUSCO 新聞紙・雑誌ストッカー", image: "" },
-    { url: "https://amzn.to/4qRjRBS", title: "ナカノ スタイリング タントN", image: "" },
-    { url: "https://amzn.to/3M6E5br", title: "有機黒ごまペースト", image: "" },
-    { url: "https://amzn.to/4rp4BvU", title: "キッチン・ドレイン・ネオ", image: "" },
-    { url: "https://amzn.to/4qKMznP", title: "馬油シャンプー 詰替", image: "" },
-    { url: "https://amzn.to/3LMrlXA", title: "Amazon商品", image: "" }
+    {
+        url: "https://rpx.a8.net/svt/ejp?a8mat=4AV5OF+1VA04Y+2HOM+6I9N5&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4779e.5d30c5ba.0eb4779f.b871e4e3%2Fa26011467448_4AV5OF_1VA04Y_2HOM_6I9N5%3Fpc%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F",
+        title: "楽天トラベル",
+        image: "https://hbb.afl.rakuten.co.jp/hsb/0ea7f9a4.79280dcb.0ea7f99d.1ac92fca/153145/"
+    },
+    {
+        url: "https://rpx.a8.net/svt/ejp?a8mat=4AV5OF+1VA04Y+2HOM+656YP&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0ea62065.34400275.0ea62066.204f04c0%2Fa26011467448_4AV5OF_1VA04Y_2HOM_656YP%3Fpc%3Dhttp%253A%252F%252Fwww.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Fm.rakuten.co.jp%252F",
+        title: "楽天市場",
+        image: "https://hbb.afl.rakuten.co.jp/hsb/0ec09ba3.bc2429d5.0eb4bbaa.95151395/"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_127-208%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+        title: "丸亀製麺 1,000円ギフト",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?me_id=1405809&item_id=10000090&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Frakkenshop%2Fcabinet%2F10345128%2F11800662%2Fimgrc0093020218.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_111-157%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+        title: "コメダ珈琲店 1,000円ギフト",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?me_id=1405809&item_id=10000017&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Frakkenshop%2Fcabinet%2F10345128%2F10405894%2F02.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_120-186%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+        title: "タリーズ 1,000円ギフト",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?me_id=1405809&item_id=10000056&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Frakkenshop%2Fcabinet%2F10345128%2F10788053%2Fimgrc0089973014.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_112-237%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+        title: "大戸屋 2,000円ギフト",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?me_id=1405809&item_id=10000115&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Frakkenshop%2Fcabinet%2F10345128%2F10345136%2F2000yen_127188.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://item.rakuten.co.jp/kouragumi/201175/",
+        title: "骨取りサバ切身",
+        image: "https://thumbnail.image.rakuten.co.jp/@0_mall/kouragumi/cabinet/10531638/imgrc0114997850.jpg?_ex=128x128"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd9f2a.b9209cb6.50cd9f2b.14e117e9/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fmakuraseizou%2Fs003%2F&link_type=picttext",
+        title: "そば殻枕",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd9f2a.b9209cb6.50cd9f2b.14e117e9/?me_id=1337128&item_id=10000107&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fmakuraseizou%2Fcabinet%2Fnarajirou%2Fimgrc0169565820.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cda52b.4cf3b824.50cda52c.5d187712/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fgeneless%2F03-0112yh%2F&link_type=picttext",
+        title: "カーディガン",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cda52b.4cf3b824.50cda52c.5d187712/?me_id=1334588&item_id=10002249&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fgeneless%2Fcabinet%2Fshouhin01%2F03line%2Fygh_01%2F03-0112-00.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cda82a.411ce472.50cda82b.360f1622/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fa-price%2F4571495432325%2F&link_type=picttext",
+        title: "冷蔵庫",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cda82a.411ce472.50cda82b.360f1622/?me_id=1243088&item_id=10975178&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fa-price%2Fcabinet%2Fdesign%2F20%2F0-4571495434251.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cdc9eb.ebe26f21.50cdc9ec.83ea9686/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffordear-nizimusubi%2Fb0dsj82zvx%2F&link_type=picttext",
+        title: "プロテイン",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cdc9eb.ebe26f21.50cdc9ec.83ea9686/?me_id=1437967&item_id=10087846&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Ffordear-nizimusubi%2Fcabinet%2Frakub220_0017%2Fb0dsj82zvx00.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://rpx.a8.net/svt/ejp?a8mat=4AV5OF+1VA04Y+2HOM+6I9N5&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4779e.5d30c5ba.0eb4779f.b871e4e3%2Fa26011467448_4AV5OF_1VA04Y_2HOM_6I9N5%3Fpc%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F",
+        title: "楽天トラベル",
+        image: "https://hbb.afl.rakuten.co.jp/hsb/0ea7f9a4.79280dcb.0ea7f99d.1ac92fca/153145/"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_127-208%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+        title: "丸亀製麺 1,000円ギフト",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?me_id=1405809&item_id=10000090&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Frakkenshop%2Fcabinet%2F10345128%2F11800662%2Fimgrc0093020218.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_111-157%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+        title: "コメダ珈琲店 1,000円ギフト",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?me_id=1405809&item_id=10000017&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Frakkenshop%2Fcabinet%2F10345128%2F10405894%2F02.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_120-186%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+        title: "タリーズ 1,000円ギフト",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?me_id=1405809&item_id=10000056&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Frakkenshop%2Fcabinet%2F10345128%2F10788053%2Fimgrc0089973014.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
+    {
+        url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_112-237%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+        title: "大戸屋 2,000円ギフト",
+        image: "https://hbb.afl.rakuten.co.jp/hgb/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?me_id=1405809&item_id=10000115&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Frakkenshop%2Fcabinet%2F10345128%2F10345136%2F2000yen_127188.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
+    },
 ];
 
 interface HeaderAdsProps {
