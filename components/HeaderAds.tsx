a70b306f3c5de4da2b7782aeda328b04961e7847
OSAFUNE <pcjidouka@gmail.com>
2026-02-09 05:35:17 +0900

    Update data 2026/02/09

--- components/HeaderAds.tsx
+++ components/HeaderAds.tsx
@@ -5,11 +5,6 @@ const AMAZON_LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/
 // 商品情報（楽天おすすめ情報）
 const AD_ITEMS = [
     {
-        url: "https://rpx.a8.net/svt/ejp?a8mat=4AV5OF+1VA04Y+2HOM+6I9N5&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4779e.5d30c5ba.0eb4779f.b871e4e3%2Fa26011467448_4AV5OF_1VA04Y_2HOM_6I9N5%3Fpc%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F",
-        title: "楽天トラベル",
-        image: "https://hbb.afl.rakuten.co.jp/hsb/0ea7f9a4.79280dcb.0ea7f99d.1ac92fca/153145/"
-    },
-    {
         url: "https://rpx.a8.net/svt/ejp?a8mat=4AV5OF+1VA04Y+2HOM+656YP&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0ea62065.34400275.0ea62066.204f04c0%2Fa26011467448_4AV5OF_1VA04Y_2HOM_656YP%3Fpc%3Dhttp%253A%252F%252Fwww.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Fm.rakuten.co.jp%252F",
         title: "楽天市場",
         image: "https://hbb.afl.rakuten.co.jp/hsb/0ec09ba3.bc2429d5.0eb4bbaa.95151395/"
@@ -59,11 +54,7 @@ const AD_ITEMS = [
         title: "プロテイン",
         image: "https://hbb.afl.rakuten.co.jp/hgb/50cdc9eb.ebe26f21.50cdc9ec.83ea9686/?me_id=1437967&item_id=10087846&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Ffordear-nizimusubi%2Fcabinet%2Frakub220_0017%2Fb0dsj82zvx00.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
     },
-    {
-        url: "https://rpx.a8.net/svt/ejp?a8mat=4AV5OF+1VA04Y+2HOM+6I9N5&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4779e.5d30c5ba.0eb4779f.b871e4e3%2Fa26011467448_4AV5OF_1VA04Y_2HOM_6I9N5%3Fpc%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F",
-        title: "楽天トラベル",
-        image: "https://hbb.afl.rakuten.co.jp/hsb/0ea7f9a4.79280dcb.0ea7f99d.1ac92fca/153145/"
-    },
+
     {
         url: "https://hb.afl.rakuten.co.jp/ichiba/50cd90e5.ba52a2fd.50cd90e6.09db1cba/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Frakkenshop%2Frakken_127-208%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
         title: "丸亀製麺 1,000円ギフト",
