9ad481ca94664082b83ffbab78b6aed0f81549ee
OSAFUNE <pcjidouka@gmail.com>
2026-03-04 16:58:42 +0900

    Update thumbnail object-fit to cover

--- pages/index.tsx
+++ pages/index.tsx
@@ -560,7 +560,7 @@ export default function Home() {
         .post-thumbnail-card img {
           width: 100%;
           height: 100%;
-          object-fit: contain; /* Ensure full image is visible */
+          object-fit: cover;
           display: block;
         }
 
